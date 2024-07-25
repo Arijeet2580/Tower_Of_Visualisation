4f09c00e27e17bde3383fb1feba597111112c421
Arijeet2580 <kararijeet26@gmail.com>
2024-07-25 19:12:28 +0530

    Updated the algorithm for Tower of Hanoi

--- dialog.js
+++ dialog.js
@@ -1,2 +1,14 @@
 const NumberOfBlock = document.getElementById('BlockNo').innerHTML;
-console.log(NumberOfBlock);
+const NumberOfPass  = document.getElementById('NoOfPass'); 
+function Hanoi(NumberOfBlock) {
+    if (NumberOfBlock<=1) {
+        return 1;
+    }
+    return (2*(Hanoi(NumberOfBlock-1)))+1;
+}
+
+const Run = document.getElementById("Run");
+
+Run.addEventListener("click",()=>{
+    NumberOfPass.innerHTML=Hanoi(NumberOfBlock);
+});
