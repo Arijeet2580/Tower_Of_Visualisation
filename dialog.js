496c6c69a1ed320f07b15e9eaf2c1ed1580404f0
Arijeet2580 <kararijeet26@gmail.com>
2024-07-25 19:25:01 +0530

    Updated the hanoi Function

--- dialog.js
+++ dialog.js
@@ -1,14 +1,14 @@
 const NumberOfBlock = document.getElementById('BlockNo').innerHTML;
 const NumberOfPass  = document.getElementById('NoOfPass'); 
-function HanoiFun(NumberOfBlock) {
+function HanoiFunc(NumberOfBlock) {
     if (NumberOfBlock<=1) {
         return 1;
     }
-    return (2*(HanoiFun(NumberOfBlock-1)))+1;
+    return (2*(HanoiFunc(NumberOfBlock-1)))+1;
 }
 
 const Run = document.getElementById("Run");
 
 Run.addEventListener("click",()=>{
-    NumberOfPass.innerHTML=HanoiFun(NumberOfBlock);
+    NumberOfPass.innerHTML=HanoiFunc(NumberOfBlock);
 });
